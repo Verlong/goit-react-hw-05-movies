851b0411593938a19f07cf7974b2c1240808ff82
Maryna Bonko <bonko23@gmail.com>
2023-06-08 21:45:11 +0300

    add input

--- src/api/get-api-key.js
+++ src/api/get-api-key.js
@@ -1,7 +1,14 @@
+const PER_PAGE = 10;
 export default async function getMovie(inputValue, page = 1) {
   const API_KEY = '60dca8f10f0481ca0ad574dccc048134';
-  const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+  const BASE_URL = 'https://api.themoviedb.org/3/';
 
-  return await fetch(`${BASE_URL}?${inputValue}&${API_KEY}`);
+  return await fetch(
+    `${BASE_URL}?q=${inputValue}&page=${page}&key=${API_KEY}&per_page=${PER_PAGE}`
+  ).then(res => res.json());
 }
-// with_cast;
+// 'https://api.themoviedb.org/3/trending/movie/day?language=en-US'
+// 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1'
+// 'https://api.themoviedb.org/3/movie/movie_id?language=en-US'
+// 'https://api.themoviedb.org/3/movie/movie_id/credits?language=en-US'
+// 'https://api.themoviedb.org/3/movie/movie_id/reviews?language=en-US&page=1'
--- src/pages/movie/movies.jsx
+++ src/pages/movie/movies.jsx
@@ -1,12 +1,39 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 
 const Movies = () => {
   //запрос
   //useEffect(()=> {http query? if it nead}, [])
+  const [movies, setMovies] = useState([
+    'mov1',
+    'mov2',
+    'mov3',
+    'mov4',
+    'mov5',
+    'mov6',
+  ]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const movieId = searchParams.get('movieId') ?? '';
 
+  const updateQueryString = event => {
+    const movieIdValue = event.target.value;
+    if (movieIdValue === '') {
+      return setSearchParams({});
+    }
+    setSearchParams({ movieId: movieIdValue });
+
+    // const nextParams = name !== '' ? { name } : {};
+    // setSearchParams(nextParams);
+  };
+
+  const visibleMovie = movies.filter(movie => movie.includes(movieId));
   return (
     <>
-      {['mov-1', 'mov-2', 'mov-3', 'mov-4', 'mov-5'].map(movie => {
+      <input type="text" value={movieId} onChange={updateQueryString} />
+      <button type="submit" onClick={() => setSearchParams({ c: 'hello' })}>
+        🔎Search
+      </button>
+      {visibleMovie.map(movie => {
         return (
           <Link key={movie} to={`${movie}`}>
             <ul>
